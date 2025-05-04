268db519e9f89aea4f102bb9067eafffe9d83053
HarikaS21 <152512826+HarikaS21@users.noreply.github.com>
2025-05-04 18:01:05 +0530

    Update SavedResumes.jsx

--- frontend/src/pages/SavedResumes.jsx
+++ frontend/src/pages/SavedResumes.jsx
@@ -15,7 +15,7 @@ const SavedBuilder = () => {
           return;
         }
 
-        const response = await axios.get('http://localhost:5000/api/resume/load', {
+        const response = await axios.get('https://mangodb-sbqv.onrender.com/api/resume/load', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
